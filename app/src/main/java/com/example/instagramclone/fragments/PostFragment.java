65a3fc38f7984d97d549075815c245d1f2f1a367
jjp6315 <jjp6315@psu.edu>
2021-03-23 22:51:43 -0400

    done

--- app/src/main/java/com/example/instagramclone/fragments/PostFragment.java
+++ app/src/main/java/com/example/instagramclone/fragments/PostFragment.java
@@ -31,8 +31,8 @@ public class PostFragment extends Fragment {
 
     public static final String TAG = "PostFragment";
     private RecyclerView rvPosts;
-    private PostsAdapter adapter;
-    private List<Post> allPosts;
+    protected PostsAdapter adapter;
+    protected List<Post> allPosts;
 
     public PostFragment() {
         // Required empty public constructor
@@ -61,9 +61,11 @@ public class PostFragment extends Fragment {
         queryPosts();
     }
 
-    private void queryPosts() {
+    protected void queryPosts() {
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
         query.include(Post.KEY_USER);
+        query.setLimit(20);
+        query.addDescendingOrder(Post.KEY_CREATED_KEY);
         query.findInBackground(new FindCallback<Post>() {
             @Override
             public void done(List<Post> posts, ParseException e) {
